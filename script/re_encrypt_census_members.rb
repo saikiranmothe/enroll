70853f30a433cbf22cf17064e6e06ba0c67ca1e8
Trey Evans <lewis.r.evans@gmail.com>
2016-08-10 15:49:44 -0400

    Created employee migration script.

---
+++ script/re_encrypt_census_members.rb
@@ -0,0 +1,35 @@
+re_encrypted_people = 0
+
+total_people_count = CensusEmployee.all.count
+CensusEmployee.all.each do |pers|
+  encrypted_ssn = pers.encrypted_ssn
+  begin
+    if !pers.encrypted_ssn.blank?
+      ssn_value = SymmetricEncryption.secondary_ciphers.first.decrypt(encrypted_ssn) rescue nil
+      if !ssn_value.blank? && (ssn_value =~ /[0-9]{9}/)
+        pers.ssn = ssn_value
+      end
+    end
+    pers.census_dependents.each do |ver|
+      if !ver.encrypted_ssn.blank?
+        dec_ver_ssn = SymmetricEncryption.secondary_ciphers.first.decrypt(ver.encrypted_ssn) rescue nil
+        if !dec_ver_ssn.blank? && (dec_ver_ssn =~ /[0-9]{9}/)
+          ver.encrypted_ssn = SymmetricEncryption.encrypt(dec_ver_ssn)
+        end
+      end
+    end
+    pers.save!
+    re_encrypted_people = re_encrypted_people + 1
+  rescue OpenSSL::Cipher::CipherError
+    puts pers.id
+    re_encrypted_people = re_encrypted_people + 1
+  rescue => e
+    puts e.inspect
+    puts SymmetricEncryption.secondary_ciphers.first.decrypt(encrypted_ssn)
+    puts SymmetricEncryption.decrypt(encrypted_ssn)
+    puts pers.id
+  end
+  if (re_encrypted_people % 1000) == 0
+    puts "#{re_encrypted_people}/#{total_people_count}"
+  end
+end
