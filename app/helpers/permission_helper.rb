17103a5c354b2685570f54bea08e744b4049e54f
Trey Evans <lewis.r.evans@gmail.com>
2016-08-25 17:24:51 -0400

    Suggestions for block helpers.

--- app/helpers/permission_helper.rb
+++ app/helpers/permission_helper.rb
@@ -3,6 +3,8 @@
 # https://github.com/elabs/pundit/issues/339
 # https://www.relishapp.com/rspec/rspec-rails/v/3-0/docs/view-specs/view-spec#passing-view-spec-that-stubs-a-helper-method
 module PermissionHelper
+  include Haml::Helpers
+
   def policy_helper pundit_object
     policy(pundit_object) 
   end
@@ -17,5 +19,18 @@ module PermissionHelper
     raw result
   end
 
+  def h_pundit_span(pundit_object, pundit_method)
+    permission_cls = policy_helper(pundit_object).send(pundit_method) ? 'no-op' : 'blocking'
+    haml_tag("span.#{permission_cls}") do
+      yield
+    end
+  end
+
+  def e_pundit_span(pundit_object, pundit_method, &blk)
+    permission_cls = policy_helper(pundit_object).send(pundit_method) ? 'no-op' : 'blocking'
+    content_tag("span", :class => permission_cls) do
+      yield
+    end
+  end
 
 end  
