27398f7751ad0038503fce3a354c978a64f4ca02
Sagar Arlekar <sagar.arlekar@gmail.com>
2016-09-01 15:09:33 -0400

    Refs #8033 refactored controller. changed queue name.

--- app/controllers/events/census_employees_controller.rb
+++ app/controllers/events/census_employees_controller.rb
@@ -5,56 +5,46 @@ module Events
     def resource(connection, delivery_info, properties, body)
       reply_to = properties.reply_to
       headers = properties.headers || {}
-      ssn = headers.stringify_keys["ssn"]
-      dob =  headers.stringify_keys["dob"]
-      first_name = headers.stringify_keys["first_name"]
-      last_name = headers.stringify_keys["last_name"]
 
-      census_employees = find_census_employee({ssn:ssn, dob:dob, first_name:first_name })
+      begin
+        ssn, dob = ssn_and_dob(body)
+        census_employees = find_census_employee({ssn: ssn, dob: dob})
 
-      if !census_employees.nil?
-        begin
-          response_payload = render_to_string "events/employer_response/employer_response", :formats => ["xml"], :locals => { :census_employees => census_employees }
-          reply_with(connection, reply_to, policy_id, "200", response_payload)
-        rescue Exception => e
-          reply_with(
-            connection,
-            reply_to,
-            "500",
-            JSON.dump({
-               exception: e.inspect,
-               backtrace: e.backtrace.inspect
-            })
-          )
+        return_status = "200"
+        if census_employees.empty?
+          return_status = "404"
         end
-      else
-        reply_with(connection, reply_to, "404", "")
+
+        response_payload = render_to_string "events/census_employee/employer_response", :formats => ["xml"], :locals => {:census_employees => census_employees}
+        reply_with(connection, reply_to, return_status, response_payload)
+      rescue Exception => e
+        reply_with(connection, reply_to, "500", JSON.dump({exception: e.inspect, backtrace: e.backtrace.inspect}))
       end
     end
 
-    def reply_with(connection, reply_to, return_status, body, eligibility_event_kind = nil)
+    private
 
-      headers = { 
-              :return_status => return_status
-      }
-
-      if !eligibility_event_kind.blank?
-        headers[:eligibility_event_kind] = eligibility_event_kind
-      end
+    def reply_with(connection, reply_to, return_status, body)
+      headers = {:return_status => return_status}
 
       with_response_exchange(connection) do |ex|
-        ex.publish(
-          body,
-          {
-            :routing_key => reply_to,
-            :headers => headers
-          }
-        )
+        ex.publish(body, {:routing_key => reply_to, :headers => headers })
       end
     end
 
     def find_census_employee(options)
-      CensusEmployee.where(encrypted_ssn: CensusMember.encrypt_ssn(ssn)).and(dob:option[:dob])
+      CensusEmployee.where(encrypted_ssn: CensusMember.encrypt_ssn(options[:ssn])).and(dob: Date.strptime(options[:dob], "%Y%m%d"))
+    end
+
+    def ssn_and_dob(xml)
+      request_hash = parse_request(xml)
+      ssn = request_hash[:request][:parameters][:ssn]
+      dob = request_hash[:request][:parameters][:dob]
+      [ssn, dob]
+    end
+
+    def parse_request(xml)
+      Parsers::Xml::Cv::EmployerRequestParser.parse(xml)
     end
   end
 end
